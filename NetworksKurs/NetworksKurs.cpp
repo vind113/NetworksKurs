b71f56eff45e0e9c25bdce985947cf00e485df32
Yarmoshchuk Dima <dima.yarmoshchuk@gmail.com>
2020-11-18 20:44:54 +0200

    basic socket server and client

--- NetworksKurs/NetworksKurs.cpp
+++ NetworksKurs/NetworksKurs.cpp
@@ -7,20 +7,16 @@
 
 using namespace std;
 
+// -------------------------------------------------------- Введення ----------------------------------------------------------
 // A - повідомлення вводиться з клавіатури.
-string consoleRead() {
+string readConsole() {
     string input;
     getline(cin, input, ';');
     return input;
 }
 
-// B - повідомлення виводиться у файл.
-void consoleWrite(string text) {
-    std::cout << text;
-}
-
 // B - повідомлення зчитується з файлу.
-string fileRead(string path) {
+string readFile(string path) {
     ifstream readFileStream(path);
     string line;
     string result;
@@ -31,16 +27,45 @@ string fileRead(string path) {
     return result;
 }
 
-// B - повідомлення зчитується з файлу.
+// C - перша частина повідомлення вводиться з клавіатури, а друга частина зчитується з файлу.
+string readConsoleAndFile(string path) {
+    return readConsole() + readFile(path);
+}
+
+// D - частина повідомлення зчитується з одного файлу, а частина з іншого.
+string readTwoFiles(string path1, string path2) {
+    return readFile(path1) + readFile(path2);
+}
+
+// E - перша частина повідомлення зчитується з файлу, а друга частина вводиться з клавіатури.
+string readFileAndConsole(string path) {
+    return readFile(path) + readConsole();
+}
+
+// ------------------------------------------------------- Виведення ---------------------------------------------------------
+// A - повідомлення виводиться на екран і у файл.
+void consoleAndFileWrite(string text, string path) {
+    consoleWrite(text);
+    fileWrite(text, path);
+}
+
+// B - повідомлення виводиться у файл.
 void fileWrite(string text, string path) {
     ofstream outputFileStream(path);
     outputFileStream << text;
     outputFileStream.close();
 }
 
+// C - повідомлення виводиться на екран.
+// D - повідомлення виводиться на екран.
+// E - повідомлення виводиться на екран.
+void consoleWrite(string text) {
+    std::cout << text;
+}
+
 int main()
 {
     string path = "file.txt";
     fileWrite("Hello from file", path);
-    consoleWrite(fileRead(path));
+    consoleWrite(readFile(path));
 }
